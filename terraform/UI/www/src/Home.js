bbe9c4d037ef3edf9933fe8af470bb642092f265
ojrprice <ojrprice@gmail.com>
2017-03-21 17:08:41 +0000

    test
    testing

--- terraform/UI/www/src/Home.js
+++ terraform/UI/www/src/Home.js
@@ -12,7 +12,7 @@ class Home extends Component {
                         <div className="l-box">
                             <h3 className="information-head">Get started today</h3>
                             <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+                                Ollie doing stuff
                             </p>
                         </div>
                     </div>
